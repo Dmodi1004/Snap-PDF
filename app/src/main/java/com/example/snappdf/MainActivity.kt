69c1844059e88fc25d637ac5f5394d512a2ffb66
Dhruv <d.modi1004@gmail.com>
2023-12-01 11:20:15 +0530

    second commit

--- app/src/main/java/com/example/snappdf/MainActivity.kt
+++ app/src/main/java/com/example/snappdf/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.snappdf
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.snappdf.Adapter.HomeAdapter
 import com.example.snappdf.Adapter.LAYOUT_BOD
+import com.example.snappdf.Repository.MainRepo
+import com.example.snappdf.Utils.MyResponses
+import com.example.snappdf.View.MainViewModel
+import com.example.snappdf.View.MainViewModelFactory
 import com.example.snappdf.ViewModel.BooksModel
 import com.example.snappdf.ViewModel.HomeModel
 import com.example.snappdf.databinding.ActivityMainBinding
@@ -15,6 +21,13 @@ class MainActivity : AppCompatActivity() {
     private val list: ArrayList<HomeModel> = ArrayList()
     private val adapter = HomeAdapter(list, activity)
 
+    private val repo = MainRepo(activity)
+    private val viewModel by lazy {
+        ViewModelProvider(activity, MainViewModelFactory(repo))[MainViewModel::class.java]
+    }
+
+    private val TAG = "Main Activity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,40 +37,39 @@ class MainActivity : AppCompatActivity() {
 
             rvHome.adapter = adapter
 
-            val tempList = ArrayList<BooksModel>()
-            val tempModel = BooksModel(
+            viewModel.getHomeData()
+            handleHomeBackend()
+
+            /*val tempModel = BooksModel(
                 image = "https://imgs.search.brave.com/14B1ZDgcDf3m9n67U_59Huk5y42swL5lAxGeO2oKszQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDY2Njg2/ODUuanBn",
                 title = "Rich dad poor dad",
                 description = "In this book about reading, author Rohan Joshi shares his passion for reading and provides insights into the benefits of reading. The book offers practical advice on how to develop a reading habit, choose books, and make the most of reading time.",
                 author = "Robert",
                 bookPDF = "https://css4.pub/2015/icelandic/dictionary.pdf"
-            )
-            for (i in 1..5) {
-                tempList.add(
-                    tempModel
-                )
-            }
-
-            list.add(
-                HomeModel(
-                    catTitle = null, booksList = null, bod = tempModel, LAYOUT_TYPE = LAYOUT_BOD
-                )
-            )
-
-            list.add(
-                HomeModel(
-                    catTitle = "Self Improvement",
-                    booksList = tempList
-                )
-            )
-            list.add(
-                HomeModel(
-                    catTitle = "Financial Books",
-                    booksList = tempList
-                )
-            )
-
+            )*/
         }
 
+    }
+
+    private fun handleHomeBackend() {
+        viewModel.homeLiveData.observe(activity) {
+            when (it) {
+                is MyResponses.Error -> {
+                    Log.i(TAG, "handleHomeBackend: ${it.errorMessage}")
+                }
+
+                is MyResponses.Loading -> {
+                    Log.i(TAG, "handleHomeBackend: Loading...")
+                }
+
+                is MyResponses.Success -> {
+                    val tempList = it.data
+                    tempList?.forEach {
+                        list.add(it)
+                        adapter.notifyItemChanged(list.size)
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/snappdf/Repository/MainRepo.kt
+++ app/src/main/java/com/example/snappdf/Repository/MainRepo.kt
@@ -1,4 +1,50 @@
 package com.example.snappdf.Repository
 
-class MainRepo {
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import com.example.snappdf.Utils.MyResponses
+import com.example.snappdf.ViewModel.HomeModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class MainRepo(val context: Context) {
+
+    private val firebaseDatabase = FirebaseDatabase.getInstance()
+    private val databaseRef = firebaseDatabase.getReference("AppData").child("Home")
+
+    private val homeLD = MutableLiveData<MyResponses<ArrayList<HomeModel>>>()
+
+    val homeLiveData get() = homeLD
+
+    suspend fun getHomeData() {
+
+        homeLiveData.postValue(MyResponses.Loading())
+
+        databaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshots: DataSnapshot) {
+                if (!snapshots.exists()) {
+                    homeLiveData.postValue(MyResponses.Error("Data not exist."))
+                    return
+                }
+                val tempList = ArrayList<HomeModel>()
+                for (snapshot in snapshots.children) {
+                    val homeModel = snapshot.getValue(HomeModel::class.java)
+                    homeModel?.let {
+                        tempList.add(homeModel)
+                    }
+                }
+                if (tempList.size > 0)
+                    homeLiveData.postValue(MyResponses.Success(tempList))
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                homeLiveData.postValue(MyResponses.Error("Something went wrong. Please try again."))
+            }
+
+        })
+
+    }
+
 }
